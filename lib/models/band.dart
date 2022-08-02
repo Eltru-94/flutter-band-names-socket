abcf7490fd6d5b36bcfc7b873bcddefa2cf18158
Alexander Torres <alexskate19@gmail.com>
2022-08-01 23:17:25 -0500

    fin de la seccion 2

---
+++ lib/models/band.dart
@@ -0,0 +1,12 @@
+class Band {
+  late String id;
+  late String name;
+  late int votes;
+  Band({this.id = "", this.name = "", this.votes = 0});
+
+  factory Band.fromMap(Map<String, dynamic> obj) => Band(
+        id: obj['id'],
+        name: obj['name'],
+        votes: obj['votes'],
+      );
+}
